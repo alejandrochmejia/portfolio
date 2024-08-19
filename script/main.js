9a8af6c8f47fb51bff50615bd7754cd63162c4e2
alejandrochmejia <alejandrochmejia@gmail.com>
2024-08-19 18:43:16 -0400

    adding sun in dark mode

--- script/main.js
+++ script/main.js
@@ -5,9 +5,11 @@ function changeColorMode(){
     document.documentElement.style.setProperty('--bg','#262626');
     document.documentElement.style.setProperty('--color','#f0f1f2');
     bandColor = false;
+    document.getElementById('colorChange').name = 'sunny'
     } else {
         document.documentElement.style.setProperty('--color','#262626');
         document.documentElement.style.setProperty('--bg','#f0f1f2');
         bandColor = true;
+        document.getElementById('colorChange').name = 'moon'
     }
 }
