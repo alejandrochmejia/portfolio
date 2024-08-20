54bbfaa74ebfbcecc1658cfe7e14a1a4d9894baf
alejandrochmejia <alejandrochmejia@gmail.com>
2024-08-20 14:43:16 -0400

    favicon added

--- script/main.js
+++ script/main.js
@@ -2,14 +2,17 @@ let bandColor = true
 
 function changeColorMode(){
     if (bandColor) {
-    document.documentElement.style.setProperty('--bg','#262626');
-    document.documentElement.style.setProperty('--color','#f0f1f2');
-    bandColor = false;
-    document.getElementById('colorChange').name = 'sunny'
+        document.documentElement.style.setProperty('--bg','#262626');
+        document.documentElement.style.setProperty('--color','#f0f1f2');
+        bandColor = false;
+        document.getElementById('colorChange').name = 'sunny'
     } else {
         document.documentElement.style.setProperty('--color','#262626');
         document.documentElement.style.setProperty('--bg','#f0f1f2');
         bandColor = true;
         document.getElementById('colorChange').name = 'moon'
     }
+}
+function deselect(){
+    element = document.getElementsByClassName('navSelected');
 }
